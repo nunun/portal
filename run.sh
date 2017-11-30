f290ece42b81b1e85200a453bff1af95663a5353
nunun <hoge.life@gmail.com>
2017-11-30 16:50:23 +0000

    slate

--- run.sh
+++ run.sh
@@ -1,17 +1,22 @@
 CWD=$(cd `dirname ${0}`; pwd)
+FQDN="${1:-"fu-n.net"}"
+COMMAND="${2:-""}"
 cd "${CWD}"
 
 docker-compose down
-[ "${1}" = "down" ] && exit 0
+[ "${COMMAND}" = "down" ] && exit 0
 
 # https://hub.docker.com/r/jojomi/hugo/
+COMPOSE_PATH="${CWD}"
+[ "${OSTYPE}" = "cygwin" ] && COMPOSE_PATH="`cygpath -w ${CWD}`"
 docker run --rm \
-        -v ${CWD}/src:/src \
-        -v ${CWD}/html:/output \
-        -e HUGO_THEME=hugo_theme_pickles \
-        -e HUGO_BASEURL=http://fu-n.net/ \
+        -v ${COMPOSE_PATH}/src:/src \
+        -v ${COMPOSE_PATH}/html:/output \
+        -v ${COMPOSE_PATH}/src/config.${FQDN}.toml:/src/config.toml \
+        -e HUGO_THEME=slate \
+        -e HUGO_BASEURL=http://${FQDN}/ \
         jojomi/hugo
 docker-compose build
-[ "${1}" = "build" ] && exit 0
+[ "${COMMAND}" = "build" ] && exit 0
 
 docker-compose up
