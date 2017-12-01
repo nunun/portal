55e13a0d112afdf2eda7193ff6716116c29bce26
nunun <hoge.life@gmail.com>
2017-12-01 16:35:28 +0900

    update configs

--- run.sh
+++ run.sh
@@ -1,22 +1,24 @@
 CWD=$(cd `dirname ${0}`; pwd)
-FQDN="${1:-"fu-n.net"}"
-COMMAND="${2:-""}"
+COMMAND="${1:-"up"}"
+ENVIRONMENT="${2:-"local"}"
 cd "${CWD}"
+set -e
 
-docker-compose down
-[ "${COMMAND}" = "down" ] && exit 0
+up() { down; build; docker-compose up; }
+down() { docker-compose down; }
+build() { hugo; docker-compose build; }
+push() { build; docker-compose push; }
+hugo() {
+        # https://hub.docker.com/r/jojomi/hugo/
+        local project_path="${CWD}"
+        local config_path="${project_path}/src/config.${ENVIRONMENT}.toml"
+        [ "${OSTYPE}" = "cygwin" ] && project_path="`cygpath -w ${CWD}`"
+        [ ! -f "${config_path}" ] && echo "'${config_path}' not found?" && exit 1
+        docker run --rm \
+                -v ${project_path}/src:/src \
+                -v ${project_path}/html:/output \
+                -v ${project_path}/src/config.${ENVIRONMENT}.toml:/src/config.toml \
+                jojomi/hugo
+}
 
-# https://hub.docker.com/r/jojomi/hugo/
-COMPOSE_PATH="${CWD}"
-[ "${OSTYPE}" = "cygwin" ] && COMPOSE_PATH="`cygpath -w ${CWD}`"
-docker run --rm \
-        -v ${COMPOSE_PATH}/src:/src \
-        -v ${COMPOSE_PATH}/html:/output \
-        -v ${COMPOSE_PATH}/src/config.${FQDN}.toml:/src/config.toml \
-        -e HUGO_THEME=slate \
-        -e HUGO_BASEURL=http://${FQDN}/ \
-        jojomi/hugo
-docker-compose build
-[ "${COMMAND}" = "build" ] && exit 0
-
-docker-compose up
+${COMMAND}
