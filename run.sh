b0c83cf21c020057f277ba0ffa4589b7719431ce
nunun <hoge.life@gmail.com>
2017-12-04 15:31:35 +0900

    prepare for virtual host

--- run.sh
+++ run.sh
@@ -7,27 +7,29 @@ cd "${CWD}"
 
 task_up() { task_down; task_build; docker-compose up; }
 task_down() { docker-compose down; }
-task_build() { task_hugo; docker-compose build; }
+task_build() { task_hugo; ENVIRONMENT="${ENVIRONMENT}" docker-compose build; }
 task_hugo() {
-        local config_toml="config.${ENVIRONMENT}.toml"
-        echo "build hugo site with config '${config_toml}' ..."
+        for v in "portal" "www"; do
+                local config_toml="config.${ENVIRONMENT}.toml"
+                echo "build hugo site '${v}' with config '${config_toml}' ..."
 
-        # check paths
-        local project_path="${CWD}/nginx"
-        local config_path="src/${config_toml}"
-        if [ ! -f "${project_path}/${config_path}" ]; then
-                echo "config file '${config_path}' not found?"
-                exit 1
-        fi
+                # check paths
+                local project_path="${CWD}/nginx"
+                local config_path="src/${v}/${config_toml}"
+                if [ ! -f "${project_path}/${config_path}" ]; then
+                        echo "config file '${config_path}' not found?"
+                        exit 1
+                fi
 
-        # https://hub.docker.com/r/jojomi/hugo/
-        [ "${OSTYPE}" = "cygwin" ] \
-                && project_path="`cygpath -w ${project_path}`"
-        docker run --rm \
-                -v ${project_path}/src:/src \
-                -v ${project_path}/html:/output \
-                -v ${project_path}/${config_path}:/src/config.toml \
-                jojomi/hugo
+                # https://hub.docker.com/r/jojomi/hugo/
+                [ "${OSTYPE}" = "cygwin" ] \
+                        && project_path="`cygpath -w ${project_path}`"
+                docker run --rm \
+                        -v ${project_path}/src/${v}:/src \
+                        -v ${project_path}/html/${v}:/output \
+                        -v ${project_path}/${config_path}:/src/config.toml \
+                        jojomi/hugo
+        done
 }
 task_publish() {
         ENVIRONMENT="${PUBLISH_ENVIRONMENT}"
